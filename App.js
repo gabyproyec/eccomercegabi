008b12d54867e64df964e15619ac5d4029f1230d
miecommerce <projectgabyozu@gmail.com>
2022-10-27 00:26:20 -0500

    mi primer comer

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Switch, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import ProductsList from './components/ProductsLists';
 
 export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-300 dark:bg-black ">
+      <View className="flex-row w-full gap-5">
+       <Text className="dark:text-white text-2xl font-bold">
+        holagaby!
+       </Text>
+       <Switch value ={colorScheme ==="dark"} onChange={toggleColorScheme}/>
+       
+      </View>
+      <ProductsList />
+      <StatusBar style={colorScheme ==="dark" ? "light" :"dark"}/>
+      
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
